fb70f5e65a981279ca60e199c0e721206c53b92f
a612f <84751058+a612f@users.noreply.github.com>
2021-09-30 23:33:55 -0500

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,25 @@
+class Drops {
+    constructor(x,y) {
+        var options = {
+            friction:0.001,
+            restitution:0.1
+        }
+
+        this.drop = Bodies.circle(x,y,5,options);
+        this.radius = 5;
+        World.add(world, this.drop)
+    }
+
+    display(){
+    fill("blue");
+    ellipseMode(CENTER);
+    ellipse(this.drop.position.x,this.drop.position.y,this.radius,this.radius)
+    }
+
+    update(){
+        if(this.drop.position.y > height){
+            Matter.Body.setPosition(this.drop,{x:random(0,400), y:random(0,400)})
+
+        }
+    }
+}
